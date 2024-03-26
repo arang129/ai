e3ece1308c1baf500b957bd727ae41e0fa451095
arang129 <48246455+arang129@users.noreply.github.com>
2024-03-26 14:30:34 +0800

    Update and rename nextweb.py to ai.py

--- ai.py
+++ ai.py
@@ -7,21 +7,21 @@ from copy import copy
 from pathlib import Path
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-__version__ = '0.15'
+__version__ = '0.01'
 
 # This is the entry point for jupyter-server-proxy . The packaging metadata
 # tells it about this function. For details, see:
 # https://jupyter-server-proxy.readthedocs.io/en/latest/server-process.html
-def setup_nextweb():
+def setup_ai():
     # Using a Unix socket prevents other users on a multi-user system from accessing
     # our server. The alternative is a TCP socket ('-p', '{port}').
     return {
-        'command': [sys.executable, '-m', 'nextweb', '-u', '{unix_socket}'],
+        'command': [sys.executable, '-m', 'ai', '-u', '{unix_socket}'],
         'unix_socket': True,
         'launcher_entry': {
             'enabled': True,
-            'icon_path': '/opt/tljh/hub/share/jupyterhub/next.svg',
-            'title': 'NextChat',
+            'icon_path': '/opt/tljh/hub/share/jupyterhub/ai.svg',
+            'title': 'AI',
         },
     }
 
@@ -68,11 +68,18 @@ TEMPLATE = """\
 <!DOCTYPE html>
 <html>
 <head>
-	<title>NextChat</title>
+	<title>AI Chat</title>
 </head>
  
 <body>
-<iframe src="https://chatgpt.yunlab.synology.me/" width="100%" height="870px" frameborder="0"></iframe>
+    <script type="module">
+        import Chatbot from "https://cdn.jsdelivr.net/npm/flowise-embed/dist/web.js";
+        Chatbot.initFull({
+          chatflowid: "6a4c827b-ac4c-4263-9ef9-ac129245b836",
+          apiHost: "https://flowiseai.yunlab.synology.me",
+        });
+      </script>
+      <flowise-fullchatbot></flowise-fullchatbot>
 </body>
 </html>
 """
