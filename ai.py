6c71ad56b7937d4666cf01db3be82d02ebd232a8
arang129 <48246455+arang129@users.noreply.github.com>
2024-03-26 15:30:58 +0800

    Update ai.py

--- ai.py
+++ ai.py
@@ -7,7 +7,7 @@ from copy import copy
 from pathlib import Path
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-__version__ = '0.031'
+__version__ = '0.01'
 
 # This is the entry point for jupyter-server-proxy . The packaging metadata
 # tells it about this function. For details, see:
@@ -72,19 +72,14 @@ TEMPLATE = """\
 </head>
  
 <body>
-<script
-  data-host-id="1"
-  data-auto-reg="true"
-  data-login-token=""
-  data-close-width="52"
-  data-close-height="52"
-  data-open-width="380"
-  data-open-height="680"
-  data-position="right"
-  data-welcome="Welcome to YunLab. If you have any questions, please contact me."
-  src="https://chat.yunlab.synology.me/widget.js"
-  async
-></script>
+<flowise-fullchatbot></flowise-fullchatbot>
+<script type="module">
+    import Chatbot from "https://cdn.jsdelivr.net/npm/flowise-embed/dist/web.js"
+    Chatbot.initFull({
+        chatflowid: "6a4c827b-ac4c-4263-9ef9-ac129245b836",
+        apiHost: "https://flowiseai.yunlab.synology.me",
+    })
+</script>
 </body>
 </html>
 """
